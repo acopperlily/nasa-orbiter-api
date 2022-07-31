e770b1d6b389752c1b31f9f4fae284efdacbc4a3
acopperlily <acopperlily@yahoo.com>
2022-07-31 05:24:19 -0400

    Create new file for object info

---
+++ fleet.js
@@ -0,0 +1,34 @@
+const vehicles = {
+  columbia: {
+    name: 'Columbia',
+    serialNum: 'OV-102',
+    numFlights: 28,
+  },
+  atlantis: {
+    name: 'Atlantis',
+    serialNum: 'OV-104',
+    numFlights: 33,
+  },
+  challenger: {
+    name: 'Challenger',
+    serialNum: 'OV-099',
+    numFlights: 10,
+  },
+  discovery: {
+    name: 'Discovery',
+    serialNum: 'OV-103',
+    numFlights: 39,
+  },
+  endeavour: {
+    name: 'Endeavour',
+    serialNum: 'OV-105',
+    numFlights: 25,
+  },
+  enterprise: {
+    name: 'Enterprise',
+    serialNum: 'OV-101',
+    numFlights: 5,
+  },
+};
+
+module.exports = {vehicles};
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const {vehicles} = require('./fleet.js');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -5,39 +6,39 @@ const PORT = 1981;
 
 app.use(cors());
 
-const vehicles = {
-  "columbia": {
-    "name": "Columbia",
-    "serialNum": "OV-102",
-    "numFlights": 28
-  },
-  "atlantis": {
-    "name": "Atlantis",
-    "serialNum": "OV-104",
-    "numFlights": 33
-  },
-  "challenger": {
-    "name": "Challenger",
-    "serialNum": "OV-099",
-    "numFlights": 10
-  },
-  "discovery": {
-    "name": "Discovery",
-    "serialNum": "OV-103",
-    "numFlights": 39
-  },
-  "endeavour": {
-    "name": "Endeavour",
-    "serialNum": "OV-105",
-    "numFlights": 25
-  },
-  "enterprise": {
-    "name": "Enterprise",
-    "serialNum": "OV-101",
-    "numFlights": 5
-  }
+// const vehicles = {
+//   "columbia": {
+//     "name": "Columbia",
+//     "serialNum": "OV-102",
+//     "numFlights": 28
+//   },
+//   "atlantis": {
+//     "name": "Atlantis",
+//     "serialNum": "OV-104",
+//     "numFlights": 33
+//   },
+//   "challenger": {
+//     "name": "Challenger",
+//     "serialNum": "OV-099",
+//     "numFlights": 10
+//   },
+//   "discovery": {
+//     "name": "Discovery",
+//     "serialNum": "OV-103",
+//     "numFlights": 39
+//   },
+//   "endeavour": {
+//     "name": "Endeavour",
+//     "serialNum": "OV-105",
+//     "numFlights": 25
+//   },
+//   "enterprise": {
+//     "name": "Enterprise",
+//     "serialNum": "OV-101",
+//     "numFlights": 5
+//   }
 
-};
+// };
 
 app.get('/', (req, res) => {
   app.set('view engine', 'html');
